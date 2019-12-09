aafa7777856e4ee9a8d6e34c47e27dc16ae0acb6
Nekaiya Wilson <nekaiya.wilson@bison.howard.edu>
2019-12-09 11:59:09 -0500

    Initial commit

---
+++ src/test/java/com/learningbycoding/springboot_3_01/Springboot301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningbycoding.springboot_3_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot301ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
